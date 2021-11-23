c57e0e7a52482dd671162cefec0715aa2d36cddd
chieutrangg <chieutrangg@gmail.com>
2021-11-24 00:41:05 +0200

    heading

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,79 @@ import styled from 'styled-components'
 
 function Header() {
     return (
-        <Container>
-            
-        </Container>
+        <Nav>
+            <Logo src ="\images\logo.svg" />
+            <NavMenu>
+                <a>
+                    <img src="/images/home-icon.svg" />
+                    <span>HOME</span>
+                </a>
+                <a>
+                    <img src="/images/search-icon.svg" />
+                    <span>SEARCH</span>
+                </a>
+                <a>
+                    <img src="/images/watchlist-icon.svg" />
+                    <span>WATCHLIST</span>
+                </a>
+                <a>
+                    <img src="/images/original-icon.svg" />
+                    <span>ORIGINALS</span>
+                </a>
+                <a>
+                    <img src="/images/movie-icon.svg" />
+                    <span>MOVIES</span>
+                </a>
+                <a>
+                    <img src="/images/series-icon.svg" />
+                    <span>SERIES</span>
+                </a>
+
+            </NavMenu>
+            <UserImg src="/images/userlogo.jpg" />
+        </Nav>
     )
 }
 
 export default Header
-const Container = styled.div`
+const Nav = styled.nav`
     height: 70px;
     background: #090b13;
+    display: flex;
+    algin-item: center;
+    padding: 0 36px;
+`
+const Logo = styled.img`
+    width : 80px;
+`
+const NavMenu = styled.div`
+    display: flex;
+    flex: 1;
+    margin-left: 25px;
+    align-items: center;
+    a {
+        display:flex;
+        align-items:center;
+        padding: 0 20px;
+        cursor: pointer;
+        img {
+            height: 20px;
+        }
+        span {
+            font-size: 13px;
+            letter-spacing: 1.42px;
+            &:after {
+                content:"";
+                height: 2px;
+                background: white;
+            }
+        }
+    }
+`
+const UserImg = styled.img`
+    width: 48px;
+    height: 48px;
+    border-radius: 50%;
+    cursor: pointer;
+    margin-top: 10px
 `
