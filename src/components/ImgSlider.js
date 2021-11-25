4271126b240000b497fb4caed1cab4ba59b20f62
chieutrangg <chieutrangg@gmail.com>
2021-11-25 20:00:44 +0200

    complete slider

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -13,14 +13,21 @@ function ImgSlider() {
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
+        // variableWidth: true,
     };
     return (
         <Carousel {...settings}>
             <Wrap>
-                <img src ="/images/slider-badging.jpg"  />
+                <img src ="\images\slider-badging.jpg" />
             </Wrap>
             <Wrap>
-                <img src ="/images/slider-badag.jpg"  />
+                <img src ="\images\slider-badag.jpg" />
+            </Wrap>
+            <Wrap>
+                <img src ="\images\slider-badging.jpg" />
+            </Wrap>
+            <Wrap>
+                <img src ="\images\slider-badag.jpg" />
             </Wrap>
             
         </Carousel>
@@ -30,18 +37,40 @@ function ImgSlider() {
 export default ImgSlider
 
 const Carousel = styled(Slider)`
-    margin-top: 20px;    
+    margin: 20px 50px 0 55px;  
+    padding: 0 5px 0 20px;
+    min-width:0;
+    min-height:0;
+
+    ul li button{
+         &:before {
+             font-size: 10px;
+             color: rgb(150, 150, 171);
+         }
+     }
+    li.slick-active button:before {
+        color:white;
+    }
 
     .slick-list{
         overflow: visible;
     }
+    button {
+        z-index: 1;
+    }
 `
 const Wrap = styled.div`
-    // img{
-    //     border-radius: 4px;
-    //     width: 100%;
-    //     height: 100%;
-    //     box-shadow: rgb(0 0 0/ 69%) 0px 26px 30px -10px,
-    //     rgb(0 0 0 / 73%) 0px 16px 10px -10px;
-    // }
+    cursor: pointer;
+    img{
+        border-radius: 4px solid transparent;
+        width: 99%;
+        height: 100%;
+        box-shadow: rgb(0 0 0/ 69%) 0px 26px 30px -10px,
+        rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+        transition-duration: 300ms;
+        
+        &:hover {
+            border: 4px solid rgba(249, 249, 249, 0.9);
+        }
+    }
 `
