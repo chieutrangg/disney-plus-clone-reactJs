b68b46116fc1a4eb25e067e11f8ad41b81cff765
chieutrangg <chieutrangg@gmail.com>
2021-11-26 01:23:23 +0200

    recommended component

---
+++ src/components/Movies.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Movies() {
+    return (
+        <Container>
+            <h2>Recommended for You</h2>
+            <Content>
+
+            </Content>
+        </Container>
+    )
+}
+
+export default Movies
+
+const Container = styled.div`
+    position: relative;
+    margin: 300px 70px 0 70px;
+   
+`
+
+const Content = styled.div`
+`
